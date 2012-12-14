0a243fb6b0ef4dbc1823a0d0d45ea507efa1ba00
Arin Sarkissian <arin@jeraff.com>
2012-12-13 17:39:33 -0800

    fuck with jetty connector. should put all this crap into config soon

--- src/main/java/com/jeraff/patricia/PatriciaServer.java
+++ src/main/java/com/jeraff/patricia/PatriciaServer.java
@@ -2,17 +2,26 @@ package com.jeraff.patricia;
 
 import com.jeraff.patricia.handler.rest.ApiHandler;
 import com.jeraff.patricia.handler.webui.freemarker.WebHandler;
+import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
 import org.limewire.collection.CharSequenceKeyAnalyzer;
 import org.limewire.collection.PatriciaTrie;
 
 public class PatriciaServer {
     public static void main(String[] args) throws Exception {
         final PatriciaTrie<String, String> patriciaTrie = new PatriciaTrie<String, String>(new CharSequenceKeyAnalyzer());
-        final Server server = new Server(8666);
+        final Server server = new Server();
+
+        final SelectChannelConnector connector0 = new SelectChannelConnector();
+        connector0.setPort(8666);
+        connector0.setMaxIdleTime(3000);
+        connector0.setRequestHeaderSize(8192);
+        connector0.setAcceptors(20);
+        connector0.setAcceptQueueSize(100);
 
         final ContextHandler restHandler = new ContextHandler("/api");
         restHandler.setResourceBase(".");
@@ -27,6 +36,7 @@ public class PatriciaServer {
         final ContextHandlerCollection contexts = new ContextHandlerCollection();
         contexts.setHandlers(new Handler[]{restHandler, webUIHandler});
 
+        server.setConnectors(new Connector[]{connector0});
         server.setHandler(contexts);
         server.start();
         server.join();
